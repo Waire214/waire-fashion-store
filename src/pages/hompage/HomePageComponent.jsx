db63755b3972ff5eae5a359ff2e4beb672aea0ea
waire <waire.tega@gmail.com>
2021-12-12 15:19:12 +0100

    broken the homepage into smaller components

---
+++ src/pages/hompage/HomePageComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import DirectoryComponent from '../../components/directory/DirectoryComponent'
+import './HomePageStyles.scss'
+
+function HomePageComponent() {
+    return (
+        <div className='homepage'>
+            <DirectoryComponent />
+        </div>
+    )
+}
+
+export default HomePageComponent
