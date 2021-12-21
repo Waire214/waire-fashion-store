ff91a3aea03b4bad4e51f219a9238218a279ad0f
waire <waire.tega@gmail.com>
2021-12-21 16:57:56 +0100

    setting up to the latest react router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import HomePageComponent from './pages/hompage/HomePageComponent';
+import { Routes, Route } from 'react-router-dom'
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
+
 function App() {
   return (
     <div>
-      <HomePageComponent />
+      <Routes>
+        <Route path='/' element={<HomePageComponent />} />
+        <Route path='/hats' element={ <HatsPage /> } />
+        <HomePageComponent />
+      </Routes>
     </div>
   );
 }
